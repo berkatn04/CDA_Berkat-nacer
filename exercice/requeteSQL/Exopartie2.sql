3e7ea4538ce492877aa3cc067e873a5af2c650e5
berkatn04 <berkat.n@hotmail.com>
2021-10-14 17:20:06 +0200

    14/10/2021 17:20:05,70

--- exercice/requeteSQL/Exopartie2.sql
+++ exercice/requeteSQL/Exopartie2.sql
@@ -1,90 +1,107 @@
--- Rechercher le prénom des employés et le numéro de la région de leur département. 
+-- 1/Rechercher le prénom des employés et le numéro de la région de leur département. 
 
 SELECT prenom, noregion, nodep FROM employe
 INNER JOIN dept ON employe.nodep = dept.nodept
 
--- Rechercher le numéro du département, le nom du département, le nom des employés classés par numéro de département (renommer les tables utilisées). 
+-- 2/Rechercher le numéro du département, le nom du département, le nom des employés classés par numéro de département (renommer les tables utilisées). 
 
 SELECT nodept, dept.nom as nomDept, employe.nom as nomEmp FROM dept 
 INNER JOIN employe ON dept.nodept = employe.nodep
 ORDER BY nodept
 
--- Rechercher le nom des employés du département Distribution. 
+-- 3/Rechercher le nom des employés du département Distribution. 
 
 SELECT nodept, employe.nom as nomEmp, dept.nom as nomDept FROM dept 
 INNER JOIN employe ON dept.nodept = employe.nodep
 WHERE dept.nom= "distribution"
 
--- Rechercher le nom et le salaire des employés qui gagnent plus que leur patron, et le nom et le salaire de leur patron. 
+-- 4/Rechercher le nom et le salaire des employés qui gagnent plus que leur patron, et le nom et le salaire de leur patron. 
 
-SELECT nom, salaire FROM employee 
-WHERE 
+Select employe.nom, employe.salaire, boss.nom, boss.salaire
+from employe inner join employe as boss on employe.nosup = boss.noemp
+where employe.salaire>boss.salaire
 
--- Rechercher le nom et le titre des employés qui ont le même titre que Amartakaldire. 
+-- 5/Rechercher le nom et le titre des employés qui ont le même titre que Amartakaldire. 
 
 SELECT nom, titre FROM employe
 WHERE titre in (SELECT titre FROM employe WHERE nom="Amartakaldire" )
 
--- Rechercher le nom, le salaire et le numéro de département des employés qui gagnent plus qu'un seul employé du département 31, classés par numéro de département et salaire. 
+-- 6/Rechercher le nom, le salaire et le numéro de département des employés qui gagnent plus qu'un seul employé du département 31, classés par numéro de département et salaire. 
 
 SELECT nom, salaire, nodep FROM employe
 WHERE salaire > (SELECT MIN(salaire) FROM employe WHERE nodep = 31)
+ORDER BY nodep, salaire
 
--- Rechercher le nom, le salaire et le numéro de département des employés qui gagnent plus que tous les employés du département 31, classés par numéro de département et salaire. 
+-- 7/Rechercher le nom, le salaire et le numéro de département des employés qui gagnent plus que tous les employés du département 31, classés par numéro de département et salaire. 
 
 SELECT nom, salaire, nodep FROM employe
 WHERE salaire > (SELECT MAX(salaire) FROM employe WHERE nodep = 31) 
-
---Rechercher le nom et le titre des employés du service 31 qui ont un titre que l'on trouve dans le département 32.
+ORDER BY nodep, salaire
+-- 8/Rechercher le nom et le titre des employés du service 31 qui ont un titre que l'on trouve dans le département 32.
 
 SELECT nom, titre, nodep FROM employe 
 WHERE nodep = 31 AND titre in (SELECT titre FROM employe WHERE nodep=32)
 
--- Rechercher le nom et le titre des employés du service 31 qui ont un titre l'on ne trouve pas dans le département 32.
+-- 9/Rechercher le nom et le titre des employés du service 31 qui ont un titre l'on ne trouve pas dans le département 32.
 
 SELECT nom, titre, nodep FROM employe 
 WHERE nodep = 31 AND titre NOT in (SELECT titre FROM employe WHERE nodep=32)
 
--- Rechercher le nom, le titre et le salaire des employés qui ont le même titre et le même salaire que Fairant. 
-SELECT nom, titre, salaire FROM employe 
-WHERE titre = (SELECT titre FROM employe WHERE nom= "fairent") AND salaire = ( SELECT salaire)
+-- 10/Rechercher le nom, le titre et le salaire des employés qui ont le même titre et le même salaire que Fairant. 
 
+SELECT nom, titre, salaire FROM employe
+WHERE NOT nom = 'Fairent'AND titre,salaire = (SELECT titre, salaire FROM employe WHERE nom = 'Fairent')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- 1. Calculer le nombre d'employés de chaque titre. 
+-- 11/ Calculer le nombre d'employés de chaque titre. 
 
 SELECT titre, COUNT(*)
  FROM emp
  GROUP BY titre
  
 
---  2. Calculer la moyenne des salaires et leur somme, par région.
+--  12/ Calculer la moyenne des salaires et leur somme, par région.
 
 SELECT employe.nodep, AVG(salaire), salaire, noregion FROM employe
-INNER JOIN dept ON employee.nodep = dept.nodep
+INNER JOIN dept ON employe.nodep = dept.nodep
 GROUP BY noregion
+
+-- 13/ Afficher les numéros des départements ayant au moins 3 employés. 
+
+SELECT nodep, COUNT(noemp) as "nomEmploye", noregion FROM employe
+INNER JOIN dept ON employe.nodep = dept.nodept
+GROUP BY noregion
+HAVING COUNT(noemp)> 3
+
+-- 14/ Afficher les lettres qui sont l'initiale d'au moins trois employés. 
+
+SELECT nomemp, COUNT(substring(nom,1,1)) FROM employe
+GROUP BY nomemp
+HAVING COUNT(substring(nom,1,1)) > 3
+
+-- 15/ Rechercher le salaire maximum et le salaire minimum parmi tous les salariés et l'écart entre les deux.
+
+SELECT MAX(salaire) as "salaireMAX", MIN(salaire) as "salaireMIN", (MAX(salaire)-MIN(salaire)) as "salaireDIFF" FROM employe
+
+-- 16/ Rechercher le nombre de titres différents. 
+
+SELECT COUNT(DISTINCT(titre)) as "nbreTitre" FROM employe
+
+-- 17 Pour chaque titre, compter le nombre d'employés possédant ce titre
+
+SELECT DISTINCT titre, nom, COUNT(nom) FROM employe
+GROUP BY titre
+
+-- 18/ Pour chaque nom de département, afficher le nom du département et le nombre d'employés.
+
+SELECT dept.nom, COUNT(nom) FROM employe
+INNER JOIN dept ON employe.nodep = dept.nodept
+
+
+
+
+
+
+
+
+
+
