26c2c8198f85eb82cbf5cfc76dbc525c2a01321e
berkatn04 <berkat.n@hotmail.com>
2021-10-15 10:45:06 +0200

    15/10/2021 10:45:05,48

--- exercice/requeteSQL/Exopartie2.sql
+++ exercice/requeteSQL/Exopartie2.sql
@@ -52,60 +52,64 @@ WHERE nodep = 31 AND titre NOT in (SELECT titre FROM employe WHERE nodep=32)
 SELECT nom, titre, salaire FROM employe
 WHERE NOT nom = 'Fairent'AND titre,salaire = (SELECT titre, salaire FROM employe WHERE nom = 'Fairent')
 
--- 11/ Calculer le nombre d'employés de chaque titre. 
+-- 11/Rechercher le numéro de département, le nom du département, le nom des employés, en affichant aussi les départements dans lesquels il n'y a personne, classés par numéro de département. 
 
-SELECT titre, COUNT(*)
- FROM emp
+SELECT e.nodep, d.nom AS 'nom departement', e.nom As 'nom employe'FROM employe AS e 
+RIGHT JOIN dept AS d ON e.nodep = d.nodept ORDER BY e.nodep
+
+
+-- 12/ Calculer le nombre d'employés de chaque titre. 
+
+SELECT titre, COUNT(*) FROM emp
  GROUP BY titre
  
 
---  12/ Calculer la moyenne des salaires et leur somme, par région.
+--  13/ Calculer la moyenne des salaires et leur somme, par région.
 
-SELECT employe.nodep, AVG(salaire), salaire, noregion FROM employe
-INNER JOIN dept ON employe.nodep = dept.nodep
-GROUP BY noregion
+SELECT D.noregion, SUM(E.salaire) as SalaireSomme, AVG(E.salaire) as SalaireMoy FROM employe as E 
+INNER JOIN dept as D GROUP BY D.noregion 
 
--- 13/ Afficher les numéros des départements ayant au moins 3 employés. 
+-- 14/ Afficher les numéros des départements ayant au moins 3 employés. 
 
-SELECT nodep, COUNT(noemp) as "nomEmploye", noregion FROM employe
-INNER JOIN dept ON employe.nodep = dept.nodept
-GROUP BY noregion
-HAVING COUNT(noemp)> 3
+SELECT nodepFROM employe
+GROUP BY nodep
+HAVING COUNT(nodep) >= 3
 
--- 14/ Afficher les lettres qui sont l'initiale d'au moins trois employés. 
+-- 15/ Afficher les lettres qui sont l'initiale d'au moins trois employés. 
 
-SELECT nomemp, COUNT(substring(nom,1,1)) FROM employe
-GROUP BY nomemp
-HAVING COUNT(substring(nom,1,1)) > 3
+SELECT substring(nom,1,1) AS 'initial'FROM employe
+GROUP BY initial
+HAVING  COUNT(initial) > 2
 
--- 15/ Rechercher le salaire maximum et le salaire minimum parmi tous les salariés et l'écart entre les deux.
+-- 16/ Rechercher le salaire maximum et le salaire minimum parmi tous les salariés et l'écart entre les deux.
 
 SELECT MAX(salaire) as "salaireMAX", MIN(salaire) as "salaireMIN", (MAX(salaire)-MIN(salaire)) as "salaireDIFF" FROM employe
 
--- 16/ Rechercher le nombre de titres différents. 
+-- 17/ Rechercher le nombre de titres différents. 
 
 SELECT COUNT(DISTINCT(titre)) as "nbreTitre" FROM employe
 
--- 17 Pour chaque titre, compter le nombre d'employés possédant ce titre
+-- 18/ Pour chaque titre, compter le nombre d'employés possédant ce titre
 
-SELECT DISTINCT titre, nom, COUNT(nom) FROM employe
-GROUP BY titre
+SELECT DISTINCT titre, COUNT(nom) FROM employe
+GROUP BY titre 
 
--- 18/ Pour chaque nom de département, afficher le nom du département et le nombre d'employés.
+-- 19/ Pour chaque nom de département, afficher le nom du département et le nombre d'employés.
 
 SELECT dept.nom, COUNT(nom) FROM employe
 INNER JOIN dept ON employe.nodep = dept.nodept
+GROUP BY dept.nom
+-- 20/ Rechercher les titres et la moyenne des salaires par titre dont la moyenne est supérieure à la moyenne des salaires des Représentants. 
 
--- 19/ Rechercher les titres et la moyenne des salaires par titre dont la moyenne est supérieure à la moyenne des salaires des Représentants. 
-
-SELECT titre, AVG(salaire) FROM employe 
-WHERE AVG(salaire) > (SELECT AVG(salaire) FROM employe WHERE titre="représentant")
+SELECT titre, ROUND(AVG(salaire)) as "MoySalaire" FROM employe
 GROUP BY titre
+HAVING MoySalaire > (SELECT AVG(salaire) FROM employe WHERE titre="représentant")
 
--- 20/Rechercher le nombre de salaires renseignés et le nombre de taux de commission renseignés. 
+
+-- 21/Rechercher le nombre de salaires renseignés et le nombre de taux de commission renseignés. 
 
 
-SELECT COUNT(DISCTINCT(salaire)), COUNT(DISTINCT(tauxcom)) FROM employe
+SELECT COUNT(salaire), COUNT(tauxcom) FROM employe
 
 
 
