e210266ccd8f0d8a4da63a3a2c45a40a2b87e898
59011-07-03 <59011-07-03@exchange.ad.afpanet>
2021-10-25 13:54:18 +0200

    25/10/2021 13h54

--- arretbranch.bat
+++
@@ -1,5 +0,0 @@
-echo off
-NET STOP branchCache
-NET START wampapache64
-NET START wampmysqld64
-start c:\wamp64\wampmanager.exe
--- autopush.bat
+++
@@ -1,7 +0,0 @@
-U:
-cd 59011-07-03
-git pull 
-git add . 
-git commit -m "%date% %time%"
-git push -u origin master
-pause
