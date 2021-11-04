25e00d0ee5c150d80c90dbd62e8822548323f056
berkatn04 <berkat.n@hotmail.com>
2021-11-04 12:20:16 +0100

    04/11/2021 12:20:15,22

--- C#/exercice/Program.cs
+++ C#/exercice/Program.cs
@@ -1462,11 +1462,32 @@ namespace Exercice
             }
 
             
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+           
+            PAGE 7:
 
 
-            exercice16;
+            exercice;
             */
+            int n;
+            Console.Write("Saisissez une valeur:");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
 
+                Console.WriteLine();
+                for (int j = 0; j < n; j++)
+                {
+                    if(j==0 || i==n)
+                    Console.WriteLine("* ");
+
+                }
+
+
+
+
+            }
 
 
 
