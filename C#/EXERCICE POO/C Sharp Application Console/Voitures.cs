ae60e854810903bc0cbb6d448936c5d6446e2944
berkatn04 <berkat.n@hotmail.com>
2021-11-08 17:20:08 +0100

    08/11/2021 17:20:06,36

--- C#/EXERCICE POO/C Sharp Application Console/Voitures.cs
+++ C#/EXERCICE POO/C Sharp Application Console/Voitures.cs
@@ -9,41 +9,41 @@ namespace C_Sharp_Application_Console
     class Voitures
     {
 
-        public string couleur { get; set; }
-        public string marque { get; set; }
-        public string modele{ get; set; }
-        public int nbkilometre { get; set; }
-        public string motorisation { get; set; }
+        public string Couleur { get; set; }
+        public string Marque { get; set; }
+        public string Modele{ get; set; }
+        public int NBkilometre { get; set; }
+        public string Motorisation { get; set; }
 
         public Voitures(string couleur, string marque, string modele, int nbkilometre, string motorisation)
         {
-            this.couleur = couleur;
-            this.marque = marque;
-            this.modele = modele;
-            this.nbkilometre = nbkilometre;
-            this.motorisation = motorisation;
+            Couleur = couleur;
+            Marque = marque;
+            Modele = modele;
+            NBkilometre = nbkilometre;
+            Motorisation = motorisation;
         }
        
         public override string ToString()
         {
-            return " Cette voiture est une " + this.modele + " de la marque " + this.marque + " , de couleur " + this.couleur + " , de motorisation" +
-                 this.motorisation + " , avec " + this.nbkilometre + " Kilomètres ";
+            return " Cette voiture est une " + Modele + " de la marque " + Marque + " , de couleur " + Couleur + " , de motorisation" +
+                 Motorisation + " , avec " + NBkilometre + " Kilomètres ";
         }
 
         public Voitures(string modele, int nbkilometre)
         {
-            this.modele = modele;
-            this.nbkilometre = nbkilometre;
+            Modele = modele;
+            NBkilometre = nbkilometre;
         }
 
         public Voitures(string modele, string couleur)
         {
-            this.couleur = couleur;
-            this.modele = modele;
+            Couleur = couleur;
+            Modele = modele;
         }
 
 
 
-        public int Rouler(int kilotrajet) => this.nbkilometre += kilotrajet;
+        public int Rouler(int kilotrajet) => NBkilometre += kilotrajet;
     }
 }
