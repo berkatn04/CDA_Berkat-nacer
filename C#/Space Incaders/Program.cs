d0f084dd4d21233ad22c6797ce7ac74d8884a135
berkatn04 <berkat.n@hotmail.com>
2021-11-10 10:45:08 +0100

    10/11/2021 10:45:05,94

---
+++ C#/Space Incaders/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Space_Incaders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Spaces sp = new Spaces(8, 10);
+            Console.WriteLine(sp);
+        }
+    }
+}
---
+++ C#/Space Incaders/Spaces.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Space_Incaders
+{
+    
+
+    class Spaces
+    {
+        public int NbLignes { get; set; }
+        public int NbColonnes { get; set; }
+
+        char[,] grille;
+
+        public Spaces(int nbLignes, int nbColonnes)
+        {
+            NbLignes = nbLignes;
+            NbColonnes = nbColonnes;
+            this.grille = new char[nbLignes, nbColonnes];
+            for (int j = 0; j < NbColonnes; j++)
+            {
+
+                Console.WriteLine();
+
+                for (int i = 0; i < NbLignes; i++)
+                {
+                    if (i == 1) return (Invaders.Invaders());
+                    if (i == 0 || j == NbColonnes - 1 || j == 0 || i == NbLignes - 1) Console.Write("- ");
+                    else Console.Write("  ");
+
+
+                }
+
+            }
+
+
+        } 
+    }
+}
