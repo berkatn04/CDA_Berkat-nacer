b54b314bbb1300e6ad99a0a8c9ea5cdcd992acf1
berkatn04 <berkat.n@hotmail.com>
2021-11-16 10:45:09 +0100

    16/11/2021 10:45:07,81

--- C#/FRONT/Exercice/MainWindow.xaml.cs
+++ C#/FRONT/Exercice/MainWindow.xaml.cs
@@ -20,16 +20,108 @@ namespace Exercice
     /// </summary>
     public partial class MainWindow : Window
     {
+        int op = 0;
+        string Op;
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        
+
+        private void btnNumerique_Click(object sender, RoutedEventArgs e)
+        {
+            // sender est l'élément qui a déclenché l'événement
+            tb.Text += ((Button)sender).Content;
+        }
+
+        private void btnInit_Click(object sender, RoutedEventArgs e)
+        {
+            tb.Text = "";
+        }
+
+        private void btnOP_Click(object sender, RoutedEventArgs e)
+        {
+            string vide = "";           
+            if (tb.Text != vide)
+                tb.Text += (string)((Button)sender).Content;
+
+            else
+                MessageBox.Show("Veulliez entre une valeur d'abord");
+
+
+        }
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            double w = Application.Current.MainWindow.Width;
+            double h = Application.Current.MainWindow.Height;
+            tb.Text = "w" + w + ", h" + h;
+            btn1.FontSize = h / 20; btn2.FontSize = h / 20; btn3.FontSize = h / 20; btn4.FontSize = h / 20; btn5.FontSize = h / 20;
+            btn6.FontSize = h / 20; btn7.FontSize = h / 20; btn8.FontSize = h / 20; btn9.FontSize = h / 20; btnVirgule.FontSize = h / 20;
+            btnPlus.FontSize = h / 20; btnMoins.FontSize = h / 20; btnMultiplier.FontSize = h / 20; btnDiviser.FontSize = h / 20; btnEgale.FontSize = h / 20;
+            btnCE.FontSize = h / 20;
+        }
+        
+        static double calculSimple(double valeur1, string operateur, double valeur2)
+        {
+            double resultat = valeur1;
+            switch (operateur)
+            {
+                case "+":
+                    resultat = valeur1 + valeur2;
+                    break;
+                case "-":
+                    resultat = valeur1 - valeur2;
+                    break;
+                case "x":
+                    resultat = valeur1 * valeur2;
+                    break;
+                case "/":
+                    if(valeur2 !=0)
+                    resultat = valeur1 / valeur2;
+                    else
+                    MessageBox.Show("Division impossible par 0");
+                    break;
+                case "$":
+                    resultat = Math.Pow(valeur1, valeur2);
+                    break;
+                default:
+                    break;
+            }
+            return resultat;
+        }
+
+        private void operateur()
         {
             
+            if (tb.Text.Contains("+"))
+                op = tb.Text.IndexOf("+");
+                Op = tb.Text.Substring(op, 1);
+            if (tb.Text.Contains("-"))
+                op = tb.Text.IndexOf("-");
+                Op = tb.Text.Substring(op, 1);
+            if (tb.Text.Contains("x"))
+                op = tb.Text.IndexOf("x");
+                Op = tb.Text.Substring(op, 1);
+            if (tb.Text.Contains("/"))
+                op = tb.Text.IndexOf("/");
+                Op = tb.Text.Substring(op, 1);
+        }
+       
+        private void btn_resultat(object sender, RoutedEventArgs e)
+        {
+            operateur();
+            double val1;
+            double val2;
+            val1 = Convert.ToDouble(tb.Text.Substring(0, op));
+            val2 = Convert.ToDouble(tb.Text.Substring(op + 1, tb.Text.Length - op - 1));
+            tb.Text = Convert.ToString(calculSimple(val1, Op, val2));
+
+
+
         }
 
         
+
     }
 }
