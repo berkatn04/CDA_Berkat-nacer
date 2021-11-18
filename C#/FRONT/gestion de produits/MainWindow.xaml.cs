de1ba26e59216c6624b08f918e8c4d5eeb836ca5
berkatn04 <berkat.n@hotmail.com>
2021-11-18 10:45:21 +0100

    18/11/2021 10:45:14,64

--- C#/FRONT/gestion de produits/MainWindow.xaml.cs
+++ C#/FRONT/gestion de produits/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using gestiondeproduits;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,19 +18,69 @@ using System.Windows.Shapes;
 
 namespace gestion_de_produits
 {
-    /// <summary>
-    /// Logique d'interaction pour MainWindow.xaml
-    /// </summary>
+ 
     public partial class MainWindow : Window
     {
+        List<Produits> liste;
+        string path = @"../../../Produits.json";
         public MainWindow()
         {
             InitializeComponent();
+            liste = CreerListe();
+            liste = TransformeJson();
+            RemplirGrid();
+            CreerFichier();
         }
 
-        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        public void RemplirGrid()
         {
-
+            listeProduits.ItemsSource = liste;
         }
+
+        private List<Produits> CreerListe()
+        {
+            List<Produits> liste = new List<Produits>();
+            for (int i = 1; i < 10; i++)
+            {
+                Produits p1 = new Produits(i, "Produit : " + i, "Alimentaire", i + 2);
+                liste.Add(p1);
+            }
+            liste.Dump();
+            return liste;         
+        }
+
+        private void CreerFichier()
+        {
+            File.WriteAllText(path, JsonConvert.SerializeObject(liste, Formatting.Indented));
+        }
+
+        private string LireFichier()
+       
+        {
+            string chaine;
+            try
+            {
+              
+                chaine = File.ReadAllText(path);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Une exception s'est produite : " + e.Message);
+                Console.WriteLine("Indiquer le path :");
+                path = Console.ReadLine();
+                chaine = LireFichier();
+            }
+            return chaine;
+        }
+
+        private List<Produits> TransformeJson()
+        {
+            string chaine = LireFichier();
+            List<Produits> liste = JsonConvert.DeserializeObject<List<Produits>>(chaine);
+            return liste;
+        }
+
+
+
     }
 }
