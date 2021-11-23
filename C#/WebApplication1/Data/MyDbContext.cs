556c39294d53a744619cd2eb5bf4344ba4176474
berkatn04 <berkat.n@hotmail.com>
2021-11-23 10:45:18 +0100

    23/11/2021 10:45:14,97

---
+++ C#/WebApplication1/Data/MyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace WebApplication1
+
+{
+
+	public class MyDbContext : DbContext
+	{
+		public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+		{
+
+		}
+	}
+}
