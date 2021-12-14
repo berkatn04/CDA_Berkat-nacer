f0d689568a6594e7d8b488a93d9799c96667d614
Billel Berkat <berkat.n@hotmail.com>
2021-12-14 17:33:06 +0100

    ajout projet installer

---
+++ C#/APPLICATION/TestINSTALLER/ClassLibrary1/Installer1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    [RunInstaller(true)]
+    public partial class Installer1 : System.Configuration.Install.Installer
+    {
+        public Installer1()
+        {
+            InitializeComponent();
+        }
+
+        public override void Install(IDictionary savedState)
+        {
+            base.Install(savedState);
+            string param = this.Context.Parameters["base"];
+            File.WriteAllText("C:\\", param);
+        }
+    }
+}
