627970bdf342d578003c27373a93d2d5f1b9bd49
59011-07-03 <59011-07-03@exchange.ad.afpanet>
2022-01-24 14:17:50 +0100

    ajout api illevia

---
+++ WEB/PHP/API/test api illevia/PHP/View/Header.php
@@ -0,0 +1,15 @@
+
+<body>
+	<div id="page">
+		<div id="entete">
+			<div id="titre">
+				<div id="logo">
+			 	<img src="Images/plan.png" />
+				</div>
+
+				<div id="titre_page">
+                  Abonnement Illevia
+                </div>
+            </div>
+		</div>
+        <div id="contenu">
---
+++ WEB/PHP/API/test api illevia/PHP/View/Principale.php
@@ -0,0 +1,13 @@
+﻿<!-- Cette page est le contenu de la page principale du site; Elle présente la liste des parkings. -->
+
+<div id="divSousTitre">
+	<h3>Selectionner un abonnement</h3>
+</div>
+<div id="divContenu">
+	<div class="ligne titreLigne">		
+		<div class="libelle">libelle </div>
+		<div class="typetitre">Type titre produit</div>
+		<div class="prix">Prix</div>
+	</div>
+	<div class="espaceHorizon"></div>
+</div>
